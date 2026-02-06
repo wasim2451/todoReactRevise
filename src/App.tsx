e316523caee39955b9caa6f279e6a0fc375128cd
Wasim Akhtar <wasimakhtarkhan2003@gmail.com>
2026-02-06 19:16:40 +0530

    Basic Setup Done

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  //Create a Task Checker
+  const [data, setData] = useState([]);
+  return <></>;
+}
+
+export default App;
